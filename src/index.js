b708a6440c8cdfdfd0033e39a3d4e0fa45dfd654
Chris Haupt <chris@apacio.com>
2021-05-31 18:05:54 +0100

    problem seems to have been resolved, removing (hopefully) redundant code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { guests } from './data';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 ReactDOM.render(
@@ -10,11 +9,6 @@ ReactDOM.render(
       <Route exact path="/">
         <App />
       </Route>
-      {guests.map((guest) => (
-        <Route path={`/${guest.id}`}>
-          <p>guest: {guest.name}</p>
-        </Route>
-      ))}
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
